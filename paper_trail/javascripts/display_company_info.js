2c84903f578d3c22cbd5249cc5a9886beba1bea4
Devon Kristine Zuegel <devonzuegel@gmail.com>
2014-03-26 14:20:24 -0400

    Doughnut & legend for party breakdown!
    Still need to add %s, but that should be minor

--- paper_trail/javascripts/display_company_info.js
+++ paper_trail/javascripts/display_company_info.js
@@ -10,18 +10,14 @@ function party_breakdown() {
     url: 'http://transparencydata.com/api/1.0/entities.json?search=' + company_name +'&type=organization&apikey=7059228ecc7540b983aaa75883835c08',
 
     dataType: 'jsonp', success: function(json) {
-      // console.log(json);
-        if (json == []) {
-          document.getElementById("top-recipients").innerHTML = "<h2>Can't find that company</h2>"; //JSON.stringify(data);
-        } else if (company_name.length > 0)  {
-          var data = json;
-          company_id = data[0].id;
-          name = data[0].name;
-          // document.getElementById("data").innerHTML ="<b>" + company_name + "'s id: </b>" + data[0].id;
-        }
-
-        party_breakdown_graph(company_id, company_name, name);
-
+      if (json == []) {
+        document.getElementById("top-recipients").innerHTML = "<h2>Can't find that company</h2>";
+      } else if (company_name.length > 0)  {
+        var data = json;
+        company_id = data[0].id;
+        name = data[0].name;
+      }
+      party_breakdown_graph(company_id, company_name, name);
     }
 
   });
@@ -33,24 +29,58 @@ function party_breakdown_graph(company_id, company_name, name) {
       url: 'http://transparencydata.com/api/1.0/aggregates/org/' + company_id + '/recipients/party_breakdown.json?apikey=7059228ecc7540b983aaa75883835c08',
       // FOR INDIVIDUAL:   'http://transparencydata.com/api/1.0/aggregates/indiv/' + company_id + '/recipients/party_breakdown.json?apikey=7059228ecc7540b983aaa75883835c08',
       dataType: 'jsonp', success: function(json) {
-        console.log('http://transparencydata.com/api/1.0/aggregates/org/' + company_id + '/recipients/party_breakdown.json?apikey=7059228ecc7540b983aaa75883835c08');
         if (company_name.length > 0)  {
-          api_data = json;
-          add_doughnut(format_for_doughtnut(api_data));
+          var graphs_div = document.getElementById("party_breakdown");
+          clear_and_make_visible("party_breakdown");
+
+          if (isEmpty(json))  graphs_div.innerHTML += "Sorry! We can't find data for that organization."
+          else                add_doughnut(format_for_doughtnut(json), "party_breakdown");
         }
     }
   });
 }
 
-function add_doughnut(formatted_data) {
-  document.getElementById("main").innerHTML += "<canvas class=\"canvas\" id=\"canvas\" height=\"210\" width=\"210\"></canvas>";
+function clear_and_make_visible(div_id) {
+  var div = document.getElementById(div_id);
+  div.innerHTML = "";
+  div.style.display = 'inline-block';
+}
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
+
+function add_doughnut(formatted_data, div_id) {
+  var div = document.getElementById(div_id);
+
+  // add legend for doughnut
+  div.innerHTML += make_legend(formatted_data, "Party breakdown");
+
+  // add doughnut
+  div.innerHTML += "<canvas class=\"canvas\" id=\"canvas\" height=\"210\" width=\"210\"></canvas>";
   var doughnut = new Chart(document.getElementById("canvas").getContext("2d")).Doughnut(formatted_data);
+}
+
+
+function make_legend(formatted_data, title) {
+  var output = '<div class="legend"><h3>' + title + '</h3>'    +   '<table class="legend_table">';
+  for (var i = formatted_data.length - 1; i >= 0; i--) {
+    output += '<tr><td>' + color_block(formatted_data[i].color) + '</td><td>' + formatted_data[i].party + '</td></tr>';
+  }
+  output += '</table><div>';
+
+  return output;
+}
+
+function color_block(color_hex) {
+  return '<div class="color_block" style="width: 15px; height:15px; background-color:' + color_hex + '"></div>';
 }
 
 function format_for_doughtnut(api_data) {
   var data = [];
   for (var party in api_data) {
     var hash = {};
+    hash["party"] = party;
     hash["color"] = color_for(party);
     hash["value"] = parseInt(api_data[party][1]);
     data.push(hash);
@@ -106,6 +136,19 @@ function top_10_recipnts() {
   sample_graphs();
 }
 
+function print_top_recipnts(company_id, company_name, name) {
+  $.ajax({
+      url: 'http://transparencydata.com/api/1.0/aggregates/org/' + company_id + '/recipients.json?cycle=2012&apikey=7059228ecc7540b983aaa75883835c08',
+
+      dataType: 'jsonp', success: function(json) {
+        if (company_name.length > 0)  {
+          data = json;
+          document.getElementById("top-recipients").innerHTML = "<h2>" + name +"'s Top Recipients</h2>" + print_r(data, 0);
+        }
+    }
+  });
+}
+
 function print_r(to_print, level) {
   var output = '';
 
@@ -125,19 +168,6 @@ function print_r(to_print, level) {
     output += to_print;
   }
   return output;
-}
-
-function print_top_recipnts(company_id, company_name, name) {
-  $.ajax({
-      url: 'http://transparencydata.com/api/1.0/aggregates/org/' + company_id + '/recipients.json?cycle=2012&apikey=7059228ecc7540b983aaa75883835c08',
-
-      dataType: 'jsonp', success: function(json) {
-        if (company_name.length > 0)  {
-          data = json;
-          document.getElementById("top-recipients").innerHTML = "<h2>" + name +"'s Top Recipients</h2>" + print_r(data, 0);
-        }
-    }
-  });
 }
 
 function toggle_class_visib(cl){
